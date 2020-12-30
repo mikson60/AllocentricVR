5c943c060a54743cbb2cffdab00b3e9ca926b99c
Jens-Stefan <mikson60@gmail.com>
2020-12-30 13:24:58 +0200

    The FollowObject script now correctly physically follows the movement on the local axis of the target.

--- Assets/_AllocentricVR/Scripts/FollowObject.cs
+++ Assets/_AllocentricVR/Scripts/FollowObject.cs
@@ -1,30 +1,61 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class FollowObject : MonoBehaviour
 {
     [Header("Settings")] 
     [SerializeField] private float _followSpeed = 1f;
+    [SerializeField] private bool _usePhysics;
+    [SerializeField] private bool _skipFollowOnFirstUpdate = true;
     
     [Header("References")]
     [SerializeField] private Transform _target;
     [SerializeField] private Rigidbody _follower;
 
+    // Runtime
     private Vector3 _lastTargetPos = Vector3.zero;
     private Vector3 _targetPos = Vector3.zero;
+    private bool firstFrame = true;
     
-    void Update()
+    private void Update()
     {
-        Follow();
+        if (!_usePhysics)
+        {
+            Follow();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_usePhysics)
+        {
+            Follow();
+        }
     }
 
     private void Follow()
     {
+        if (_skipFollowOnFirstUpdate && firstFrame)
+        {
+            firstFrame = false;
+            _targetPos = _target.position;
+            _lastTargetPos = _targetPos;
+            return;
+        }
+        
         _targetPos = _target.position;
         
         var targetMovementInWorldSpace = _targetPos - _lastTargetPos;
         var targetMovementInLocalSpace = _target.InverseTransformDirection(targetMovementInWorldSpace); // How much did we move on our local axis?
 
-        transform.position += transform.InverseTransformDirection(targetMovementInLocalSpace) * _followSpeed;
+        if (_usePhysics)
+        {
+            _follower.velocity = transform.TransformDirection(targetMovementInLocalSpace) * _followSpeed;   
+        }
+        else
+        {
+            transform.position += transform.TransformDirection(targetMovementInLocalSpace) * _followSpeed;
+        }
         
         _lastTargetPos = _targetPos;
     }
